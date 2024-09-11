7159b6674f083575b08b6f93d503924057558bfe
tamerlankerych <157506649+tamerlankerych@users.noreply.github.com>
2024-09-11 22:35:56 +0500

    Add files via upload

---
+++ iosdevass1.swift
@@ -0,0 +1,14 @@
+var firstName: String = "Tamerlan"
+var lastName: String = "Kerimov"
+var age: Int = 20
+var birthYear: Int = 2004
+var isStudent: Bool = true
+var height: Double = 1.88
+let currentYear: Int = 2024
+let calculatedAge: Int = currentYear - birthYear
+var favoriteHobby: String = "chess"
+var numberOfHobbies: Int = 4
+var favoriteNumber: Int = 10
+var isHobbyCreative: Bool = true
+var lifeStory: String = "My name is \(firstName) \(lastName). I am \(calculatedAge) years old, born in \(birthYear). I am currently a student. I am \(height) meters tall. I enjoy \(favoriteHobby), which is a creative hobby. I have \(numberOfHobbies) hobbies in total: chess, football, basketball, and music. My favorite number is \(favoriteNumber) because of Messi."
+print(lifeStory)
